6526501eb4cfb670e3d70cf4b482046f22406242
Alvaro <beholderalv@gmail.com>
2019-02-08 12:17:02 +0100

    Multiple changes to engine

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,26 @@
+const SIZE = 60;
+const state_start = "START";//Sim initializing
+const state_next = "NEXT";//Sim running a step
+const state_end = "END";//Sim running a step
+const state_pause = "PAUSE";//Sim paused
+const state_stop = "STOP";//Sim not started
+const command_play = "PLAY";
+const command_step = "STEP";
+const command_pause = "PAUSE";
+const command_stop = "STOP";
+
 var socket = io.connect(window.location.host);
 var canvas, context;
-var simData;
+var simData = {};
+var config = {};
 var delta = 1;
-const SIZE = 30;
-
+var state = state_stop;
+var showRegions = false;
+var isDragging = false;
+var dragX;
+var dragY;
+var offsetX = 50;
+var offsetY = -120;
 var imageUser = new Image();
 imageUser.src = "images/man.png";
 var imageUser_connected = new Image();
@@ -28,8 +45,12 @@ $('#scenes').on('click', '.clickable_row', function(event) {
 		$(this).removeClass('active'); 
 	} else {
 		$(this).addClass('active').siblings().removeClass('active');
-		selectScene(1);
+		selectScene($(this).attr("id"));
 	}
+});
+
+$('#loadFile').on('click', function(event) {
+	socket.emit('config', {}, setupSim);
 });
 
 function selectScene(id){
@@ -46,52 +67,138 @@ function selectScene(id){
 		    		params[this.name] = $(this).val();
 		    	}
 		    });
-		    socket.emit('config', params);
-		    runVisual();
+		    socket.emit('config', params, setupSim);
 		});
 	});
+}
+
+function setupSim(data){
+	config.regions = data.regions;
+	config.map = {"loc": data.image_file};
+	console.log(data);
+	config.map.image = new Image();
+	config.map.image.src = config.map.loc;
+	if (config.map.image.complete) {
+	  runVisual()
+	} else {//Wait until image loads to print it
+	  config.map.image.addEventListener('load', runVisual)
+	  config.map.image.addEventListener('error', function() {
+	      alert('error')
+	  })
+	}
 }
 
 function runVisual(){
 	$.get('/visualSim', function (data) {
 		$('#container').remove();
 		$('body').append(data);
-		$('#start').on('click', function(e){
-			startSimulation();
+		$('#play').on('click', function(e){
+			sendCommand(command_play);
 			e.preventDefault();
 		});
-		$('#next').on('click', function(e){
-			nextStep();
+		$('#step').on('click', function(e){
+			sendCommand(command_step);
+			e.preventDefault();
+		});
+		$('#pause').on('click', function(e){
+			sendCommand(command_pause);
 			e.preventDefault();
 		});
 		$('#stop').on('click', function(e){
-			endSimulation();
+			sendCommand(command_stop);
 			e.preventDefault();
 		});
+		$('#toggle_regions').on('click', function(e){
+			toggleRegions();
+			e.preventDefault();
+		});
+		$('#canvas').on('click', function(e){
+			if(showRegions){
+				var pos = getMousePosCanvas(canvas,e);
+				$('#log').text(Math.round(pos.x) + ', ' + Math.round(pos.y));
+			}
+			e.preventDefault();
+		});
+		$('#canvas').on('mousedown', function(e){
+			isDragging = true;
+			dragX = e.offsetX;
+			dragY = e.offsetY;
+			console.log('Drag: ' + dragX + ','+ dragY);
+		});
+		$('#canvas').on('mousemove', function(e){
+			if(isDragging){
+				offsetX += e.offsetX - dragX;
+				offsetY += e.offsetY - dragY;
+				dragX = e.offsetX;
+				dragY = e.offsetY;
+				console.log('Offset: ' + offsetX + ','+ offsetY);
+				redraw();
+			}
+		});
+		$('#canvas').on('mouseup', function(e){
+			isDragging = false;
+		});
+		$('#canvas').on('mousewheel', function(e){
+			tmp_delta = e.originalEvent.deltaY;
+			if (tmp_delta > 0){
+				delta = delta / 1.1;
+			} else {
+				delta = delta * 1.1;
+			}
+			console.log('Zoom: ' + delta);
+			redraw();
+		});
+		$('#pause').hide();
+		$('#stop').hide();
 		canvas = document.getElementById('canvas'),
-	    context = canvas.getContext('2d');
+		context = canvas.getContext('2d');
 		window.addEventListener('resize', resizeCanvas, false);
-		window.addEventListener('keypress', nextStep, false);
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+		//window.addEventListener('keypress', nextStep, false);
+		canvas.width = window.innerWidth - 60;
+		canvas.height = window.innerHeight - 60;//Navbar height + margin
+		setDelta();
+		redraw();
 	});
 }
 
-function startSimulation(){
-	socket.emit('sim', "START");
+function toggleRegions(){
+	showRegions = !showRegions;
+	redraw();
 }
 
-function nextStep(){
-	socket.emit('sim', "NEXT");
-}
-
-function endSimulation(){
-	socket.emit('sim', "END");
-	context.clearRect(0,0,canvas.width, canvas.height);
+function sendCommand(command){
+	$('#play').hide();
+	$('#step').hide();
+	$('#pause').hide();
+	$('#stop').hide();
+	socket.emit('sim', command);
 }
 
 function info(msg){//Message INFO from engine
 	simData = msg;
+	console.log(msg);
+	if (msg.state == state_stop){
+		$('#play').show();
+		$('#step').show();
+		$('#pause').hide();
+		$('#stop').hide();
+	} else {
+		if(msg.command == command_play){
+			$('#play').hide();
+			$('#step').hide();
+			$('#pause').show();
+			$('#stop').hide();
+		} else {
+			$('#play').show();
+			$('#step').show();
+			$('#pause').hide();
+			$('#stop').show();
+		}
+	}
+	$('#log').text('Ppl:'+ (Object.keys(msg.nodes).length - 4));
+	if(msg.hasOwnProperty('step')){
+		$('#log').append('\nT:'+msg.step+'s.');
+	}
 	redraw();
 }
 
@@ -100,9 +207,31 @@ function info(msg){//Message INFO from engine
 */
 
 function redraw() {
+	context.setTransform(1, 0, 0, 1, 0, 0);
 	context.clearRect(0,0,canvas.width, canvas.height);
-	delta = canvas.height/simData.dimension;
-	for (var i = simData.nodes.length - 1; i >= 0; i--) {
+	context.setTransform(delta, 0, 0, delta, offsetX, offsetY);
+	//context.drawImage(config.map.image, 0, 0, config.map.image.width * delta, config.map.image.height * delta);
+	context.drawImage(config.map.image, 0, 0, config.map.image.width, config.map.image.height);
+	if(showRegions){
+		for (var i in config.regions) {
+			context.globalAlpha = 0.2;
+			context.fillStyle = "blue";
+			context.strokeStyle = "blue";
+			var reg = config.regions[i];
+			//context.strokeRect(delta*reg.x1, delta*reg.y1, delta*reg.x2 - delta*reg.x1, delta*reg.y2 - delta*reg.y1);
+			context.fillRect(reg.x1, reg.y1, reg.x2 - reg.x1, reg.y2 - reg.y1);
+			context.globalAlpha = 1;
+			context.lineWidth = 6;
+			context.strokeRect(reg.x1, reg.y1, reg.x2 - reg.x1, reg.y2 - reg.y1);
+			context.font = "50px Georgia";
+			context.fillStyle = "black";
+			context.fillText(reg.id,reg.x1 + 10,reg.y1 + 30);
+		}
+	}
+	if(!simData.hasOwnProperty('nodes')){
+		return;
+	}
+	for (var i in simData.nodes) {
 		var node = simData.nodes[i];
 		var image;
 		if(node.type == "PERSON"){
@@ -115,14 +244,38 @@ function redraw() {
 		} else if (node.type == "ROUTER"){
 			image = imageRouter;
 		}
-		context.drawImage(image, delta*(node.x+image.width/2),delta*(node.y+image.height/2), SIZE, image.height*SIZE/image.width);
+		var h_factor = image.height * SIZE / image.width;
+		//context.drawImage(image, delta*(node.x-SIZE/2),delta*(node.y-h_factor/2), SIZE, h_factor);
+		context.drawImage(image, node.x-SIZE/2,node.y-h_factor/2, SIZE, h_factor);
 	}
+}
+
+function  getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect(), // abs. size of element
+      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+  return {
+    x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+    y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+  }
+}
+function getMousePosCanvas(canvas, evt){
+	var imatrix = context.getTransform().inverse();
+	var pos = getMousePos(canvas, evt);
+	pos.x = pos.x * imatrix.a + pos.y * imatrix.c + imatrix.e;
+	pos.y = pos.x * imatrix.b + pos.y * imatrix.d + imatrix.f;
+	return pos;
 }
 
 function resizeCanvas() {
 	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	if (simData.dimension)
-		delta = canvas.height/simData.dimension;
+	canvas.height = window.innerHeight - 60;
+	setDelta();
 	redraw(); 
+}
+
+function setDelta(){
+	delta = 0.32; //TODO
+	//delta = Math.min(1, canvas.width / config.map.image.width, canvas.height / config.map.image.height)
 }
--- server_engine.js
+++ server_engine.js
@@ -1,6 +1,5 @@
-const name = "HYDRA";
-const version = "0.0.3.2017.05.13";
 const configurationFile = "config.json";
+const sceneConfigurationFile = "scene_config.json"
 const mason = "MASON";
 const ns3 = "NS3";
 const cmdStart = "START";
@@ -9,29 +8,50 @@ const cmdEnd = "END";
 const isMason = true;
 const isNS3 = true;
 const origin = {mason:"Mason", ns3:"ns3"};
-const scenes = [{id:0, name:"Scene 0", description:"Several users with mobile phones move around a router, connecting and disconnecting from the WiFi."},
-	{id:1, name:"Scene 1", description:"Sensing environment"},
-	{id:2, name:"Scene 2", description:"Building B at the ETSIT"},
-	{id:3, name:"Scene 3", description:"People moving in a classroom building"}];
+const state_start = "START";//Sim initializing
+const state_next = "NEXT";//Sim running a step
+const state_end = "END";//Sim running a step
+const state_pause = "PAUSE";//Sim paused
+const state_stop = "STOP";//Sim not started
+const command_play = "PLAY";
+const command_step = "STEP";
+const command_pause = "PAUSE";
+const command_stop = "STOP";
+const use_ns3 = false;
+const LEVEL_SEVERE = 0;
+const LEVEL_WARNING = 1;
+const LEVEL_INFO = 2;
+const LEVEL_FINE = 3;
+const LEVEL_FINER = 4;
+const LOG_LEVEL = LEVEL_INFO;
+const LAG_MIN = 100;
 
 var express = require('express');
 var bodyParser = require("body-parser");
 var fs = require('fs');
 var app = express();
 var http = require('http');
+var state = state_stop;
+var command;
+var step;
+var scene_config;
+
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '10mb', extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views')
 const config = JSON.parse(fs.readFileSync(configurationFile));
 const port = process.env.PORT || config.engine.port;
 
-var params = {};
-var lag, state, simData;
+var regions = {};
+var params = {loaded:false};
+var lag, scene_id;
+var index = 0;
+var nodes;
 
 app.get('/', function(req, res) {
-	res.render('index', {scenes:scenes, name:name});
+	res.render('index', {scenes:config.scenes, name:config.about.name});
 });
 
 app.post('/mason', function(req, res){
@@ -45,7 +65,8 @@ app.post('/ns3', function(req, res){
 });
 
 app.get('/scene/:id', function(req, res){
-	res.render('config', {config: config});
+	scene_id = parseInt(req.params.id);
+	res.render('config', {config: config, scene:config.scenes[scene_id]});
 });
 
 app.get('/visualSim', function(req, res){
@@ -60,91 +81,201 @@ app.get('/analysis', function(req, res){
 });
 
 const server = app.listen(port, function () {
-	console.log('----Engine listening on port '+port+'!----\n');
+	log(LEVEL_INFO, '--------------------------------------')
+	log(LEVEL_INFO, '-------RUNNING ' + config.about.name + '-------')
+	log(LEVEL_INFO, '-------VERSION ' + config.about.version + '-------');
+	log(LEVEL_INFO, '----Engine listening on port '+port+'!----');
+	log(LEVEL_INFO, '--------------------------------------\n')
 });
 
 const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
-	console.log('User connected');
+	log(LEVEL_INFO, 'User connected');
 	socket.on('disconnect', () => {
-		console.log('User disconnected');
+		log(LEVEL_INFO, 'User disconnected');
 		//endSimulation();
 	});
-	socket.on('config', function (data) {
-		params = data;
-		console.log(data);
+	socket.on('config', function (data, fn) {
+		if(Object.entries(data).length === 0 && data.constructor === Object){
+			scene_config = JSON.parse(fs.readFileSync(sceneConfigurationFile));
+			scene_id = scene_config.id;
+			params.tstep = scene_config.tstep;
+			params.initPeople = 10;
+			params.loaded = true;
+		}else{
+			params = data;
+		}
+		log(LEVEL_FINE, data);
+		fn({image_file:config.scenes[scene_id].map.image_file, regions:config.scenes[scene_id].map.regions});
 	});
-	socket.on('sim', function (data) {
-		if(data == "START"){
-			startSimulation();
-		} else if (data == "NEXT"){
-			nextStep();
-		} else if (data == "END"){
-			endSimulation();
+	socket.on('sim', function (com, fn) {
+		command = com;
+		if(state == state_stop){
+			if(com == command_play || com == command_step){
+				startSimulation();
+			}
+		} else if (state == state_pause){
+			if (com == command_play || com == command_step){
+				nextStep();
+			} else if (com == command_stop){
+				endSimulation();
+			}
 		}
 	});
 });
 
-function generateInitialPositions(){
-	var nodes =[];
-	nodes.push({id:0, type:"ROUTER", x:params.dimension/2, y:params.dimension/2});
-	for (var i = 1; i <= params.numUsers; i++) {
-		nodes.push({id:i, type:"PERSON", x:rand(0,params.dimension), y:rand(0,params.dimension)});
-	}
-	return nodes;
-}
-
 function startSimulation(){
 	lag = Date.now();
-	state = "START";
-	simData = {nodes:generateInitialPositions(), numUsers:params.numUsers, 
-		randMult: params.randMult, dimension:params.dimension, tstep: params.tstep};
+	state = state_start;
+	index = 0;
+	nodes = {};
+	step = 0;
+	var simData = {};
+	//Load regions
+	for (var i = config.scenes[scene_id].map.regions.length - 1; i >= 0; i--) {
+		var tmp_region = config.scenes[scene_id].map.regions[i];
+		regions[tmp_region.id] = tmp_region;
+	}
+	switch (scene_id) {
+		case 0: //Disconnection
+			nodes[0] = {id: index++, type:"ROUTER", x:params.dimension/2, y:params.dimension/2};
+			for (var i = 1; i <= params.numUsers; i++) {
+				nodes[i] = {id: index++, type:"PERSON", x:rand(0,params.dimension), y:rand(0,params.dimension), connected: false};
+			}
+			simData = {id: 0, nodes: nodes, numUsers:params.numUsers, randMult: params.randMult,
+				dimension:params.dimension, tstep: params.tstep};
+			break;
+		case 1: //Lift
+			nodes[0] = {id: index++, type:"ROUTER", x:400, y:400};
+			for (var i = 1; i <= params.initPeople; i++) {
+				tmp_point = getPointInRegion(2);
+				nodes[i] = {id: index++, type:"PERSON", x:tmp_point.x, y:tmp_point.y, connected:false}; 
+			}
+			simData = {id: 1, nodes: nodes, initPeople:params.initPeople, pPerMin: params.pPerMin, 
+				pLift: params.pLift, tstep: params.tstep, randMult: 0.1, numUsers:10, dimension: 200};
+			break;
+		case 2: //Classroom
+			nodes[index] = {id: index++, type:"ROUTER", x:2217, y:943};
+			nodes[index] = {id: index++, type:"ROUTER", x:4286, y:1081};
+			nodes[index] = {id: index++, type:"ROUTER", x:2210, y:2355};
+			nodes[index] = {id: index++, type:"ROUTER", x:4334, y:2013};
+			if(params.loaded){
+				Object.keys(scene_config.population.location).forEach(function(reg) {
+					for (var i = scene_config.population.location[reg] - 1; i >= 0; i--) {
+						tmp_point = getPointInRegion(reg);
+						nodes[index] = {id: index++, type:"PERSON", x:tmp_point.x, y:tmp_point.y, connected:false}; 
+					}
+				});
+			} else {
+				for (var i = 1; i <= params.initPeople; i++) {
+					for (var j = 100; j <= 1100; j += 100) {
+						tmp_point = getPointInRegion(j);
+						nodes[index] = {id: index++, type:"PERSON", x:tmp_point.x, y:tmp_point.y, connected:false}; 
+					}
+				}
+			}
+			simData = {id: 2, nodes: nodes, initPeople:params.initPeople, randMult: 0.1,
+				tstep: params.tstep, numUsers:10, dimension: 200};
+			break;
+		default:
+			//Error
+	}
+	simData.map = config.scenes[scene_id].map;
+	log(LEVEL_INFO, "Initial data generated");
+	log(LEVEL_FINE, "Initial message:", simData);
 	post(config.mason, state, simData);
-	post(config.ns3, state, simData);
-	io.sockets.emit('info', simData);
+	if (use_ns3) {
+		post(config.ns3, state, simData);
+	}
 }
 
 function nextStep(){
 	lag = Date.now();
-	state = "NEXT";
-	post(config.ns3, state, {nodes:simData.nodes, tstep: simData.tstep});
+	step++;
+	state = state_next;
+	switch (scene_id){
+		case 0:
+			break;
+		case 1:
+			removePeopleAtExits();			
+			//Generate new people
+			num_pers = params.pPerMin * params.tstep / 60000;
+			while (num_pers > 0){
+				if (num_pers >= 1 || Math.random() < num_pers){
+					tmp_point = getPointInRegion(0);
+					ind = index++;
+					nodes[ind] = {id: ind, type:"PERSON", x:tmp_point.x, y:tmp_point.y, connected:false};
+				}
+				num_pers--;
+			}
+			break;
+		case 2:
+			removePeopleAtExits();
+			break;
+	}
+	if (use_ns3) {
+		post(config.ns3, state, {nodes:nodes, tstep: params.tstep});
+	} else {
+		post(config.mason, state, {nodes:nodes, tstep: params.tstep});
+	}
 }
 
 function endSimulation(){
 	lag = Date.now();
-	state = "END";
+	state = state_end;
 	post(config.mason, state, {});
-	post(config.ns3, state, {});
+	if (use_ns3) {
+		post(config.ns3, state, {});
+	}
+	nodes = {};
 }
 
 function controller(orig, msg){
-	console.log("Lag "+orig+": ",Date.now()-lag);
-	console.log("Message at /"+orig);
+	total_lag = Date.now()-lag;
+	log(LEVEL_INFO, "Lag "+orig+": "+total_lag);
+	log(LEVEL_FINE, msg);
 	if (orig == origin.mason){
-		if(state == "NEXT"){
+		if(state == state_start){
+			state = state_pause;
+			io.sockets.emit('info', {nodes: nodes, tstep: params.tstep, state:state, command: command, step:step});
+			if(command == command_play){
+				setTimeout(nextStep, Math.max(1, 100 - total_lag));
+			}
+		} else if(state == state_next){
 			for (var i = msg.data.nodes.length - 1; i >= 0; i--) {
 				var node = msg.data.nodes[i];
-				simData.nodes[node.id].x = node.x;
-				simData.nodes[node.id].y = node.y;
+				nodes[node.id].x = node.x;
+				nodes[node.id].y = node.y;
 			}
-			io.sockets.emit('info', simData);
+			state = state_pause;
+			io.sockets.emit('info', {nodes: nodes, tstep: params.tstep, state:state, command: command, step:step});
+			if(command == command_play){
+				setTimeout(nextStep, Math.max(1, LAG_MIN - total_lag));//TODO
+			}
+		} else if (state == state_end){
+			state = state_stop;
+			io.sockets.emit('info', {state:state});
 		}
 	} else if (orig == origin.ns3){
-		if(state == "NEXT"){
+		if(state == state_start){
+
+		} else if(state == state_next){
 			for (var i = msg.data.nodes.length - 1; i >= 0; i--) {
 				var node = msg.data.nodes[i];
-				simData.nodes[node.id].connected = node.connected;
+				nodes[node.id].connected = node.connected;
 			}
-			data = {nodes: msg.data.nodes, tstep: simData.tstep}
+			var data = {nodes: msg.data.nodes, tstep: params.tstep}
 			post(config.mason, "NEXT", data);
+		} else if (state == state_end){
+			
 		}
 	}
 }
 
 function post(dest, cmd, data){
 	var msg = {cmd:cmd, data:data};
-	console.log("Sending message: ", msg.cmd, " to ", dest.name);
+	log(LEVEL_FINE, "Sending message: ", msg.cmd, " to ", dest.name);
 	var data = JSON.stringify(msg);
 	var options = {
 		host: dest.host,
@@ -159,13 +290,49 @@ function post(dest, cmd, data){
 	var req = http.request(options, function(res) {
 		res.setEncoding('utf8');
 		res.on('data', function (chunk) {
-			console.log("body: " + chunk);
+			log(LEVEL_INFO, "body: " + chunk);
 		});
 	});
 	req.write(data);
 	req.end();
 }
 
+function removePeopleAtExits(){
+	exits = config.scenes[scene_id].map.exits;
+	for (var i in nodes){
+		var node = nodes[i];
+		if (node.type == 'PERSON'){
+			for (var j = 0; j < exits.length; j++) {
+				if (isIncluded(exits[j], node)){
+					log(LEVEL_FINE, 'Node ' + node.id + ' at region ' + exits[j] + ' deleted.');
+					delete nodes[node.id];
+				}
+			}
+		}
+	}
+}
+
+function getPointInRegion(reg_id){
+	var reg = regions[reg_id];
+	x = rand(reg.x1, reg.x2);
+	y = rand(reg.y1, reg.y2);
+	return {"x":x, "y":y};
+}
+
+function isIncluded(exit_id, node){
+	region = regions[exit_id];
+	if (node.x >= region.x1 && node.x <= region.x2 && node.y >= region.y1 && node.y <= region.y2)
+		return true;
+	return false;
+}
+
 function rand(init, end){
-	return Math.round((end-init)*Math.random()*100)/100+init;
+	return Math.floor((end-init)*Math.random())+init;
+	//return Math.round((end-init)*Math.random()*100)/100+init;
+}
+
+function log(level, msg){
+	if(level <= LOG_LEVEL){
+		console.log(msg);
+	}
 }
